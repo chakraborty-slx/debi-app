b8dbda4e8fbd797151aedf21439bb320a9608d37
Abhishek Chakraborty <chakraborty@silexica.com>
2024-08-03 11:31:38 +0200

    Update firebase-config.ts

--- src/firebase/firebase-config.ts
+++ src/firebase/firebase-config.ts
@@ -1,12 +1,6 @@
 
 const config = {
-    apiKey: "AIzaSyDX9PgLbMNz0BHDW75TyXxpYOC0kFXWE6o",
-    authDomain: "debi2024.firebaseapp.com",
-    databaseURL: "https://debi2024-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "debi2024",
-    storageBucket: "debi2024.appspot.com",
-    messagingSenderId: "389019486455",
-    appId: "1:389019486455:web:133531ac3f92fab5f22bf2"
+
 };
 
 export function getFirebaseConfig() {
